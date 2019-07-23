ea2edc78b728707890d14641ad24a8e00f5b3363
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2019-07-23 20:50:52 +0200

    Add function to combine two hashes

--- emath/hash.hpp
+++ emath/hash.hpp
@@ -3,6 +3,16 @@
 #include "fwd.hpp"
 #include "int.hpp" // is_power_of_two, HUGE_PRIME_0
 
+namespace emath {
+
+/// Based on boost::hash_combine
+inline size_t combine_hashes(const size_t seed, const size_t new_hash)
+{
+    return new_hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+} // namespace emath
+
 namespace std {
 
 template<typename T>
